dbd792b422750f540fdc3ed7755a37d22fb5906a
Philopateer <pitoamir5@gmail.com>
2023-08-11 19:17:42 +0300

    Fully Functioning Table for EG stock market

--- main.js
+++ main.js
@@ -1,45 +1,77 @@
-document.getElementById("Gainers").addEventListener("click", Push_Gainers);
-document.getElementById("Losers").addEventListener("click", Push_Losers);
-document.getElementById("Sectors").addEventListener("click", Push_Losers);
-document.getElementById("Industries").addEventListener("click", Push_Gainers);
-document.getElementById("Calculator").addEventListener("click", Calculate);
-document.getElementById("news").addEventListener("click", Push_News);
+document.addEventListener('DOMContentLoaded', Push_Data);
+
+
 
 
 
 // * MUST ADD COMMENTS
 // TODO: ADD Green color to text if Change is +
-function Push_Gainers() {
-  ClearTable();
-  fetch("./data/gainers_data.json") // Replace with the correct path to your JSON file
+function Push_Data() {
+  fetch("./data/Stock_data.json") // Replace with the correct path to your JSON file
     .then((response) => response.json())
     .then((data) => {
-      const tablebody = document.getElementById("content-1");
+      const tablebody = document.getElementById("stock-data");
 
       data.forEach((item) => {
         const tr = document.createElement("tr");
-
+        tr.classList.add('hover:bg-gray-600')
         const tdCompany = document.createElement("td");
-        tdCompany.textContent = item.Name;
+        tdCompany.classList.add('px-4', 'py-4' ,'text-sm', 'font-medium' ,'whitespace-nowrap' ,'w-1/5', 'truncate')
+
+        const tdiv = document.createElement('div')
+
+        const ticker = document.createElement('h2');
+        ticker.classList.add('font-medium', 'text-gray-800' , 'dark:text-white')
+        ticker.textContent = item.Ticker
+
+        let name = document.createElement('p')
+        name.classList.add('text-sm', 'font-normal' ,'text-gray-600','dark:text-gray-400')
+        name.textContent = item.Name
+
+        if(name.textContent.includes('GENERAL COMPANY FOR LAND RECLAMATION,DEVELOPMENT')) name.textContent = 'GENERAL COMPANY FOR LAND RECLAMATION'
+        tdCompany.appendChild(tdiv)
+        tdiv.appendChild(ticker)
+        tdiv.appendChild(name)
+
+
 
         const tdPrice = document.createElement("td");
+        tdPrice.classList.add('px-4', 'py-4' ,'text-sm', 'font-medium' ,'whitespace-nowrap' , 'text-white')
         tdPrice.textContent = item.Price;
 
-        const tdPrice_chg = document.createElement("td");
+        let tdPrice_chg = document.createElement("td");
+        tdPrice_chg.classList.add('px-4', 'py-4' ,'text-sm', 'font-medium' ,'whitespace-nowrap')
+
         tdPrice_chg.textContent = item.Price_Chg;
 
         const tdPercent = document.createElement("td");
+        tdPercent.classList.add('px-4', 'py-4' ,'text-sm', 'font-medium' ,'whitespace-nowrap')
         tdPercent.textContent = item.Percent;
+        let temp = parseFloat(item.Percent.slice(0,-1))
 
         const tdVolume = document.createElement("td");
+        tdVolume.classList.add('px-4', 'py-4' ,'text-sm', 'font-medium' ,'whitespace-nowrap', 'text-white')
         tdVolume.textContent = item.Volume;
+
+        const tdRating = document.createElement('td');
+        tdRating.classList.add('px-4', 'py-4' ,'text-sm', 'font-medium' ,'whitespace-nowrap','text-white')
+        tdRating.textContent = item.Rating
+
+        if(temp > 0) {
+          tdPercent.classList.add('text-green-500')
+          tdPrice_chg.classList.add('text-green-500')
+        } else {
+          tdPercent.classList.add('text-red-500')
+          tdPrice_chg.classList.add('text-red-500')
+        }
+
 
         tr.appendChild(tdCompany);
         tr.appendChild(tdPrice);
         tr.appendChild(tdPrice_chg);
         tr.appendChild(tdPercent);
         tr.appendChild(tdVolume);
-
+        tr.appendChild(tdRating)
         tablebody.appendChild(tr);
       });
     })
@@ -47,82 +79,6 @@ function Push_Gainers() {
       console.error("Error fetching JSON data:", error);
     });
 }
-
-// * MUST ADD COMMENTS
-// TODO: ADD Green color to text if Change is +
-function Push_Losers() {
-  ClearTable();
-  fetch("./data/Losers_data.json") // Replace with the correct path to your JSON file
-    .then((response) => response.json())
-    .then((data) => {
-      const tablebody = document.getElementById("content-1");
-
-      data.forEach((item) => {
-        const tr = document.createElement("tr");
-
-        const tdCompany = document.createElement("td");
-        tdCompany.textContent = item.Name;
-
-        const tdPrice = document.createElement("td");
-        tdPrice.textContent = item.Price;
-
-        const tdPrice_chg = document.createElement("td");
-        tdPrice_chg.textContent = item.Price_Chg;
-
-        const tdPercent = document.createElement("td");
-        tdPercent.textContent = item.Percent;
-
-        const tdVolume = document.createElement("td");
-        tdVolume.textContent = item.Volume;
-
-        tr.appendChild(tdCompany);
-        tr.appendChild(tdPrice);
-        tr.appendChild(tdPrice_chg);
-        tr.appendChild(tdPercent);
-        tr.appendChild(tdVolume);
-
-        tablebody.appendChild(tr);
-      });
-    })
-    .catch((error) => {
-      console.error("Error fetching JSON data:", error);
-    });
-}
-
-// TODO: IMPELEMNT THE SECTORS SCREENER
-function Push_Sectors() {}
-// TODO: IMPLEMENT THE INDUSTRIES SCREENR
-function Push_Industries() {}
-
-function ClearTable() {
-  const table = document.getElementById("content-1");
-  table.innerHTML = " ";
-}
-
-function Calculate() {}
-
-/*    const tablebody = document.getElementById("table1");
-
-    const thead = document.createElement("thead");
-    tablebody.appendChild(thead)
-
-    const trh = document.createElement("tr")
-    const Sector = document.createElement("th")
-    Sector.textContent = "Sector"
-    const Market_cap = document.createElement("th")
-    Market_cap.textContent = "Market Cap"
-    const Dividend_Yield_FWD = document.createElement("th")
-    Dividend_Yield_FWD.textContent = "Dividend Yield FWD"
-    const Percent = document.createElement("th")
-    Percent.textContent = "Percent"
-    const Volume = document.createElement("th")
-    Volume.textContent = "Volume"
-    thead.appendChild(trh)
-    trh.appendChild(Sector)
-    trh.appendChild(Market_cap)
-    trh.appendChild(Dividend_Yield_FWD)
-    trh.appendChild(Percent)
-    trh.appendChild(Volume)*/
 
 //#00a97f for green color
 //#ff173e for red color
--- server.js
+++ server.js
@@ -1,7 +1,70 @@
 import fs from "fs";
 import puppeteer from "puppeteer";
 
-async function Get_Gainers() {
+async function Get_Stocks() {
+  // Launch the browser and open a new blank page
+  const browser = await puppeteer.launch({
+    headless: "false",
+  });
+  const page = await browser.newPage();
+
+  // Navigate the page to a URL
+  await page.goto(
+    "https://www.tradingview.com/markets/stocks-egypt/market-movers-all-stocks/"
+  );
+
+  // Wait and click on first result
+  // Click the button to load more content
+  await page.click(".loadButton-SFwfC2e0");
+  await page.waitForTimeout(4000);
+  await page.click(".loadButton-SFwfC2e0");
+  await page.waitForTimeout(4000);
+
+  const extractedData = await page.evaluate(() => {
+    //Create Empty Array to push Data into
+    const data = [];
+
+    //Get All Table ROWS
+    const tableRows = document.querySelectorAll("tbody > tr");
+
+    // Loop through each row and extract data from elements within <td> cells
+    tableRows.forEach((row) => {
+      //Get all TD cells inside each row
+      const cells = row.querySelectorAll("td");
+      //Query each td as you like in celldata object
+      const celldata = {
+        Name: cells[0].querySelector("span > sup").textContent,
+        Ticker: cells[0].querySelector("span > a").textContent,
+        Price: cells[1].textContent,
+        Percent: cells[2].textContent,
+        Price_Chg: cells[3].querySelector("span").textContent,
+        Rating: cells[4].textContent,
+        Volume: cells[5].textContent,
+        // Add more properties as needed
+      };
+
+      //Push the object into the data again
+      data.push(celldata);
+    });
+    //return the data
+    return data;
+  });
+
+  //console.log(extractedData);
+  await browser.close();
+  try {
+    const jsonData = JSON.stringify(extractedData, null, 2);
+
+    fs.writeFileSync("./data/Stock_Data.json", jsonData, "utf8");
+    console.log("Data written to Stock_Data.json");
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+Get_Stocks();
+
+/*async function Get_Gainers() {
   // Launch the browser and open a new blank page
   const browser = await puppeteer.launch({
     headless: "true",
@@ -259,7 +322,7 @@ async function Get_Sectors() {
   } catch (error) {
     console.error("Error:", error);
   }
-}
+}*/
 
 async function Get_News_TradingView() {
   // Launch the browser and open a new blank page
@@ -285,12 +348,12 @@ async function Get_News_TradingView() {
     news.forEach((el) => {
       const cell_headline = el.querySelector(".title-rY32JioV").textContent;
       const cell_source = el.querySelector(".block-TUPxzdRV").textContent;
-      const cell_link = el.getAttribute('href')
+      const cell_link = el.getAttribute("href");
 
       const celldata = {
         Headline: cell_headline,
         Source: cell_source,
-        Link: cell_link
+        Link: cell_link,
       };
       //Push the object into the data again
       data.push(celldata);
@@ -303,13 +366,12 @@ async function Get_News_TradingView() {
 
   try {
     const jsonData = JSON.stringify(extractedData, null, 2);
-  
+
     fs.writeFileSync("./data/News.json", jsonData, "utf8");
     console.log("Data written to News.json");
   } catch (error) {
     console.error("Error:", error);
   }
-
 }
 
 async function Get_News_ArabNews() {
@@ -332,11 +394,11 @@ async function Get_News_ArabNews() {
     // Loop through each row and extract data from elements within <td> cells
     news.forEach((el) => {
       const cell_headline = el.textContent;
-      const cell_link = el.getAttribute('href')
+      const cell_link = el.getAttribute("href");
       const celldata = {
         Headline: cell_headline,
         Source: "economymiddleeast",
-        Link: cell_link
+        Link: cell_link,
       };
       //Push the object into the data again
       data.push(celldata);
@@ -358,17 +420,9 @@ async function Get_News_ArabNews() {
 }
 
 //async function run() {
-await Get_Sectors();
-await Get_Industries();
-await Get_Gainers();
-await Get_Losers();
-await Get_Top_Performers()
-await Get_News_TradingView();
-await Get_News_ArabNews();
+
+//await Get_News_TradingView();
+//await Get_News_ArabNews();
 //}
 
 //setInterval(run, 900000);
-
-
-
-/*  */
